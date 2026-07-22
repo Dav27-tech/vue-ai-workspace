2b4bdccb8879ba8b03641910db5b73ae1e2084ae
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-22 16:30:45 +0200

    Components: StatisticCard - Created

---
+++ src/components/StatisticCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  value: number;
+  color: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="rounded-xl shadow-lg p-6 text-white" :class="color">
+    <h3 class="text-lg font-semibold">
+      {{ title }}
+    </h3>
+    <p class="text-4xl font-bold mt-4">
+      {{ value }}
+    </p>
+  </div>
+</template>
