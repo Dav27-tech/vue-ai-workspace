6e0df1a764edf6b474f928de6de7d1447fbd39ed
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-22 15:46:35 +0200

    Component: Welcome - Created

---
+++ src/components/Welcome.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="max-w-7xl mx-auto px-8 py-10">
+    <h2 class="text-4xl font-bold text-gray-800 mb-3">Welcome back</h2>
+    <p class="text-gray-600 text-lg">
+      Manage all your favorite AI assistants from one dashboard
+    </p>
+  </section>
+</template>
